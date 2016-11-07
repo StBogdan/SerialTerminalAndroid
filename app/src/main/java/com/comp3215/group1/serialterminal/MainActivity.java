d39adf54ee0182279b7a76e9d6d13f2fae65f03a
Bogdan <bs2u13@soton.ac.uk>
2016-11-07 13:22:47 +0000

    Added Android included USB support. Can now look for cable connection and lists endpoints.

--- app/src/main/java/com/comp3215/group1/serialterminal/MainActivity.java
+++ app/src/main/java/com/comp3215/group1/serialterminal/MainActivity.java
@@ -1,6 +1,19 @@
 package com.comp3215.group1.serialterminal;
 
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.hardware.usb.UsbAccessory;
+import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbEndpoint;
+import android.hardware.usb.UsbInterface;
+import android.hardware.usb.UsbManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,8 +22,13 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.util.HashMap;
+import java.util.Iterator;
 
 public class MainActivity extends AppCompatActivity {
+    String command;
 
     // Used to load the 'native-lib' library on application startup.
     static {
@@ -24,19 +42,83 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        // Example of a call to a native method
+        final TextView tv = (TextView) findViewById(R.id.sample_text);
+        final EditText input = (EditText) findViewById(R.id.editCommand);
+        Button  sendComand= (Button) findViewById(R.id.buttonCommand);
+        Button  usbButton  = (Button) findViewById(R.id.usbButton);
+
+        sendComand.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                command = input.getText().toString();
+                Toast.makeText(getApplicationContext(),command +"BOOP",Toast.LENGTH_LONG).show();
             }
         });
-
-        // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
         tv.setText(stringFromJNI());
+
+
+        String devicesString = "";
+        final UsbManager manager = (UsbManager) getSystemService(Context.USB_SERVICE);
+        final PendingIntent mPermissionIntent = PendingIntent.getBroadcast(this, 0, new Intent(ACTION_USB_PERMISSION), 0);
+        IntentFilter filter = new IntentFilter(ACTION_USB_PERMISSION);
+        registerReceiver(mUsbReceiver, filter);
+
+        usbButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                HashMap<String,UsbDevice> devices = manager.getDeviceList();
+                Iterator<UsbDevice> deviceIt = devices.values().iterator();
+                UsbDevice cableUSB = null ;
+                while(deviceIt.hasNext()){
+                    UsbDevice  tempUsb= deviceIt.next();
+                    if(tempUsb.getProductId() == 24596 && tempUsb.getVendorId() == 1027){
+                        Toast.makeText(getApplicationContext(), "Found USB cable", Toast.LENGTH_SHORT).show();
+                        cableUSB = tempUsb;
+                    }
+                }
+                if(cableUSB != null){
+                    manager.requestPermission(cableUSB, mPermissionIntent);
+                    tv.setText("Number of interfaces " +cableUSB.getInterfaceCount() + "\n");
+
+                    UsbInterface cableInterface = cableUSB.getInterface(0);
+                    tv.append("Number of endpoints for interface id "+  cableInterface.getId() + " is:" + cableInterface.getEndpointCount() + "\n");
+                    for(int i=0;i< cableInterface.getEndpointCount();i++){
+                        UsbEndpoint cableEndpoints =cableInterface.getEndpoint(i);
+                        tv.append(cableEndpoints.toString()+"\n"+ cableEndpoints.getDirection()+ "\n"+ cableEndpoints.getAttributes()+ "\n\n");
+                    }
+                }
+                else Toast.makeText(getApplicationContext(),"No interfaces found",Toast.LENGTH_LONG).show();
+                //Toast.makeText(getApplicationContext(),test,Toast.LENGTH_LONG).show();
+                //tv.setText(test);
+            }
+        });
     }
+
+    private static final String ACTION_USB_PERMISSION =
+            "com.android.example.USB_PERMISSION";
+    private final BroadcastReceiver mUsbReceiver = new BroadcastReceiver() {
+
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (ACTION_USB_PERMISSION.equals(action)) {
+                synchronized (this) {
+                    UsbDevice device = (UsbDevice)intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
+
+                    if (intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false)) {
+                        if(device != null){
+                            //call method to set up device communication
+                        }
+                    }
+                    else {
+                        Toast.makeText(getApplicationContext(),"Permission denied for "+ device,Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+        }
+    };
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
