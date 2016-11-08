5a537f88bd6c983a435aaba0c8d39a260bd27a1f
Bogdan <bs2u13@soton.ac.uk>
2016-11-08 21:04:25 +0000

    Added classes that implement usb serial functionality Taken from https://github.com/mik3y/usb-serial-for-android

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,12 +1,13 @@
+
+
 #include <jni.h>
 #include <string>
-
+//#include "libusb.h"
 extern "C"
 jstring
 Java_com_comp3215_group1_serialterminal_MainActivity_stringFromJNI(
         JNIEnv *env,
         jobject /* this */) {
     std::string hello = "Hello from C++";
-    for(;;);
     return env->NewStringUTF(hello.c_str());
 }
--- app/src/main/java/com/comp3215/group1/serialterminal/MainActivity.java
+++ app/src/main/java/com/comp3215/group1/serialterminal/MainActivity.java
@@ -5,30 +5,59 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.hardware.usb.UsbAccessory;
 import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbDeviceConnection;
 import android.hardware.usb.UsbEndpoint;
 import android.hardware.usb.UsbInterface;
 import android.hardware.usb.UsbManager;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.hoho.android.usbserial.driver.UsbSerialDriver;
+import com.hoho.android.usbserial.driver.UsbSerialPort;
+import com.hoho.android.usbserial.driver.UsbSerialProber;
+import com.hoho.android.usbserial.driver.util.SerialInputOutputManager;
+
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class MainActivity extends AppCompatActivity {
     String command;
+    private final ExecutorService mExecutor = Executors.newSingleThreadExecutor();
+    TextView outputDisplay= null;
+
+    private SerialInputOutputManager mSerialIoManager;
+
+    private final SerialInputOutputManager.Listener mListener =
+            new SerialInputOutputManager.Listener() {
+
+                @Override
+                public void onRunError(Exception e) {
+                  //  Log.d(TAG, "Runner stopped.");
+                }
+
+                @Override
+                public void onNewData(final byte[] data) {
+                    MainActivity.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            MainActivity.this.updateReceivedData(data);
+                        }
+                    });
+                }
+            };
+
 
     // Used to load the 'native-lib' library on application startup.
     static {
@@ -37,62 +66,127 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // Boiler plate Android
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        // Example of a call to a native method
-        final TextView tv = (TextView) findViewById(R.id.sample_text);
+        // Buttons and UI elements
+        outputDisplay = (TextView) findViewById(R.id.outputDisplay);
+        final TextView connectionDisplay = (TextView) findViewById(R.id.connectionView);
         final EditText input = (EditText) findViewById(R.id.editCommand);
-        Button  sendComand= (Button) findViewById(R.id.buttonCommand);
-        Button  usbButton  = (Button) findViewById(R.id.usbButton);
+        Button sendComand = (Button) findViewById(R.id.buttonCommand);
+        Button usbButton = (Button) findViewById(R.id.usbButton);
+        Button configButton = (Button) findViewById(R.id.buttonConfig);
+        final Button commButton = (Button) findViewById(R.id.buttonCommSend);
+
+        // USB comms
+        final UsbManager managerUSB = (UsbManager) getSystemService(Context.USB_SERVICE);
+        final HashMap<String, UsbDevice>[] devicesUSB = new HashMap[]{managerUSB.getDeviceList()};                          //Why array ? -> get past final limitation
+        final UsbDevice[] cableUSB = {null};                                                                                 //Why array ? -> get past final limitation
+        final UsbInterface[] cableInterface = {null};                                                                        //Why array ? -> get past final limitation
+        final PendingIntent mPermissionIntent = PendingIntent.getBroadcast(this, 0, new Intent(ACTION_USB_PERMISSION), 0);
+
+        IntentFilter filter = new IntentFilter(ACTION_USB_PERMISSION);
+        registerReceiver(mUsbReceiver, filter);
+
+
+
+        //Listeners
+        usbButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                devicesUSB[0] = managerUSB.getDeviceList();
+                outputDisplay.setText("Found devices:\n");
+                Iterator<UsbDevice> usbDeviceIterator = devicesUSB[0].values().iterator();
+                while (usbDeviceIterator.hasNext()) {
+                    UsbDevice tempUsb = usbDeviceIterator.next();
+                    connectionDisplay.append(tempUsb.getDeviceName() + " " + tempUsb.getVendorId() + " " + tempUsb.getProductId());
+                }
+            }
+        });
+
+        configButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Iterator<UsbDevice> deviceIt = devicesUSB[0].values().iterator();
+                while (deviceIt.hasNext()) {
+                    UsbDevice tempUsb = deviceIt.next();
+                    if (tempUsb.getProductId() == 24596 && tempUsb.getVendorId() == 1027) {         //Found the cable
+//                        Toast.makeText(getApplicationContext(), "Found USB cable", Toast.LENGTH_SHORT).show();
+                        connectionDisplay.append("Found USB cable\n");
+                        cableUSB[0] = tempUsb;
+                        managerUSB.requestPermission(cableUSB[0], mPermissionIntent);       //Get permission
+                        cableInterface[0] = cableUSB[0].getInterface(0);                    //Get interface
+
+                    }
+                }
+            }
+        });
+
+        final SerialInputOutputManager[] siom = new SerialInputOutputManager[1];
+        final UsbSerialPort[] port = new UsbSerialPort[1];
+
+        commButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // Find all available drivers from attached devices.
+                List<UsbSerialDriver> availableDrivers = UsbSerialProber.getDefaultProber().findAllDrivers(managerUSB);
+
+                if (availableDrivers.isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "No devices found", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                UsbSerialDriver driver = availableDrivers.get(0);
+                UsbDeviceConnection connection = managerUSB.openDevice(cableUSB[0]);
+
+                if (connection == null) {
+                    // You probably need to call UsbManager.requestPermission(driver.getDevice(), ..)
+                    Toast.makeText(getApplicationContext(), "Failed to get connection to device", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                // Read some data! Most have just one port (port 0).
+                port[0] = driver.getPorts().get(0);
+
+                //Toast.makeText(getApplicationContext(),driver.getPorts().size() + " test ",Toast.LENGTH_LONG).show();
+                try {
+                    port[0].open(connection);
+                    port[0].setParameters(9600, 8, UsbSerialPort.STOPBITS_1, UsbSerialPort.PARITY_NONE);
+                    siom[0] = new SerialInputOutputManager(port[0],mListener);
+                    mExecutor.submit(siom[0]);
+                } catch (IOException e) {
+                    // Deal with error.
+                }
+            }
+        });
+
 
         sendComand.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                command = input.getText().toString();
-                Toast.makeText(getApplicationContext(),command +"BOOP",Toast.LENGTH_LONG).show();
+                try {
+                    command = input.getText().toString();
+                    byte[] intCommand = new byte[commButton.length()];
+                    int i=0;
+                    for(char c : command.toCharArray()){
+                        intCommand[i]=(byte) (int) c;
+                        i++;
+                    }
+                    port[0].write(intCommand,500);
+                } catch (Exception e) {
+                    Toast.makeText(getApplicationContext(),"Something went horribly wrong with sending",Toast.LENGTH_SHORT).show();
+                    e.printStackTrace();
+                }
             }
         });
-        tv.setText(stringFromJNI());
 
 
-        String devicesString = "";
-        final UsbManager manager = (UsbManager) getSystemService(Context.USB_SERVICE);
-        final PendingIntent mPermissionIntent = PendingIntent.getBroadcast(this, 0, new Intent(ACTION_USB_PERMISSION), 0);
-        IntentFilter filter = new IntentFilter(ACTION_USB_PERMISSION);
-        registerReceiver(mUsbReceiver, filter);
+// Stop updates
+    //    mUIUpdater.stopUpdates();
+        //        port[0].close();
 
-        usbButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                HashMap<String,UsbDevice> devices = manager.getDeviceList();
-                Iterator<UsbDevice> deviceIt = devices.values().iterator();
-                UsbDevice cableUSB = null ;
-                while(deviceIt.hasNext()){
-                    UsbDevice  tempUsb= deviceIt.next();
-                    if(tempUsb.getProductId() == 24596 && tempUsb.getVendorId() == 1027){
-                        Toast.makeText(getApplicationContext(), "Found USB cable", Toast.LENGTH_SHORT).show();
-                        cableUSB = tempUsb;
-                    }
-                }
-                if(cableUSB != null){
-                    manager.requestPermission(cableUSB, mPermissionIntent);
-                    tv.setText("Number of interfaces " +cableUSB.getInterfaceCount() + "\n");
-
-                    UsbInterface cableInterface = cableUSB.getInterface(0);
-                    tv.append("Number of endpoints for interface id "+  cableInterface.getId() + " is:" + cableInterface.getEndpointCount() + "\n");
-                    for(int i=0;i< cableInterface.getEndpointCount();i++){
-                        UsbEndpoint cableEndpoints =cableInterface.getEndpoint(i);
-                        tv.append(cableEndpoints.toString()+"\n"+ cableEndpoints.getDirection()+ "\n"+ cableEndpoints.getAttributes()+ "\n\n");
-                    }
-                }
-                else Toast.makeText(getApplicationContext(),"No interfaces found",Toast.LENGTH_LONG).show();
-                //Toast.makeText(getApplicationContext(),test,Toast.LENGTH_LONG).show();
-                //tv.setText(test);
-            }
-        });
     }
 
     private static final String ACTION_USB_PERMISSION =
@@ -108,6 +202,7 @@ public class MainActivity extends AppCompatActivity {
                     if (intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false)) {
                         if(device != null){
                             //call method to set up device communication
+                            Toast.makeText(getApplicationContext(),"BOOP from "+ device,Toast.LENGTH_SHORT).show();
                         }
                     }
                     else {
@@ -147,4 +242,18 @@ public class MainActivity extends AppCompatActivity {
      * which is packaged with this application.
      */
     public native String stringFromJNI();
+
+    private void updateReceivedData(byte[] data) {
+        String message ="";
+        for(int i=0;i<data.length;i++){
+            message+= (char) data[i];
+        }
+        outputDisplay.append(message);
+      //Toast.makeText(getApplicationContext(),message,Toast.LENGTH_LONG).show();
+    }
+
+
+
+
+
 }
